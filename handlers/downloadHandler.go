37b28d2e125e256350712081eb22a91946b833fe
e-aub <eaub@proton.me>
2024-08-05 04:11:52 +0100

    method not allowed

--- handlers/downloadHandler.go
+++ handlers/downloadHandler.go
@@ -3,4 +3,9 @@ package handlers
 import "net/http"
 
 func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed"))
+		return
+	}
 }
